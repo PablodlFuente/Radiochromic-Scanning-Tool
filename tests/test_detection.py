98b03016f5b871c541f88038e3077f676eb5f4e8
PablodlFuente <pablodlfuenteantares@gmail.com>
2026-09-21 10:14:55 +0200

    Test RC child containment during movement

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -3,6 +3,7 @@ import unittest
 import cv2
 import numpy as np
 
+from custom_plugins import auto_measurements
 from custom_plugins.auto_measurements.core.detection import DetectionEngine
 from custom_plugins.auto_measurements.models import DetectionParams
 from custom_plugins.auto_measurements.ui.main_tab import AutoMeasurementsTab
@@ -36,6 +37,29 @@ class ShapeDetectionTests(unittest.TestCase):
         self.assertFalse(tab._geometry_contains(
             "rectangle", (10, 10, 60, 60), "circle", (65, 40, 20)
         ))
+
+    def test_rc_move_is_invalid_when_it_leaves_a_child_behind(self):
+        previous_overlay = auto_measurements._OVERLAY
+        self.addCleanup(setattr, auto_measurements, "_OVERLAY", previous_overlay)
+        tab = AutoMeasurementsTab.__new__(AutoMeasurementsTab)
+        tab.image_processor = type(
+            "Processor", (), {"current_image": np.zeros((120, 120, 3))}
+        )()
+        tab.tree = type(
+            "Tree", (), {
+                "get_children": lambda _self, item="": ("area",) if item == "film" else (),
+                "parent": lambda _self, item: "film" if item == "area" else "",
+            }
+        )()
+        auto_measurements._OVERLAY = {
+            "item_to_shape": {
+                "film": ("film", (60, 60, 50, 50)),
+                "area": ("circle", (30, 30, 10)),
+            },
+            "geometry_by_item": {},
+        }
+
+        self.assertFalse(tab._shape_position_is_valid("film"))
 
 
 if __name__ == "__main__":
